4e7b5d1104e3b7e323da2a5e0cdbcd9aea7c98db
user <user@user-pc>
2017-09-22 17:38:22 +0900

    0922 오예 성공

--- src/main/java/com/lol/webapp/controller/HomeController.java
+++ src/main/java/com/lol/webapp/controller/HomeController.java
@@ -1,15 +1,29 @@
 package com.lol.webapp.controller;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/index")
-	public String index() {
+	@ResponseBody
+	public String index(
+			@RequestParam(value = "n", defaultValue = "") String name) throws MalformedURLException {
 		
-		return "";
+		String key = "RGAPI-84c7e9ef-7f1f-42f5-bcb7-c1e526f80ab6";
+		String apiUrl = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/" + name + "/RiotSchmick?api_key=" + key;
+		
+		URL url = new URL(apiUrl); //<-???
+		
+		String output = String.format("n:%s", name);
+		
+		return output;
 	}
 	
 }
