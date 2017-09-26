469af381f75fae09dfeaa2d806224d2e301b3f07
user <user@user90>
2017-09-26 17:38:53 +0900

    0926

--- src/main/java/com/lol/webapp/controller/HomeController.java
+++ src/main/java/com/lol/webapp/controller/HomeController.java
@@ -1,29 +1,44 @@
 package com.lol.webapp.controller;
 
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/*")
 public class HomeController {
 	
-	@RequestMapping("/index")
-	@ResponseBody
+	@RequestMapping("index")
 	public String index(
-			@RequestParam(value = "n", defaultValue = "") String name) throws MalformedURLException {
+			@RequestParam(value = "n", defaultValue = "") String name,
+			Model model ) throws IOException  {
 		
-		String key = "RGAPI-84c7e9ef-7f1f-42f5-bcb7-c1e526f80ab6";
-		String apiUrl = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/" + name + "/RiotSchmick?api_key=" + key;
+		String json = "";
+		BufferedReader br;
+		String summonerName = "Ä·ÇÁÄ¸";
+		summonerName = URLEncoder.encode(summonerName, "UTF-8");
 		
-		URL url = new URL(apiUrl); //<-???
+		String key = "RGAPI-c489a450-db49-4935-a218-58740cd8f9dc";
+		String apiURL = "https://kr.api.riotgames.com/lol/summoner/v3/summoners/by-name/"+ summonerName +"?api_key=" + key;
 		
-		String output = String.format("n:%s", name);
+		URL url = new URL(apiURL); 
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setRequestProperty("Content-Type", "application/json");//<-???
+		br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+
+		json = br.readLine();
+		connection.disconnect();
+		model.addAttribute("info", json);
 		
-		return output;
+		return "index";
 	}
-	
 }
--- src/main/java/com/lol/webapp/entity/Info.java
+++ src/main/java/com/lol/webapp/entity/Info.java
@@ -1,5 +1,10 @@
 package com.lol.webapp.entity;
 
-public class Info {
+import org.json.JSONObject;
 
+public class Info {
+	String name = "";
+	String key = "RGAPI-c489a450-db49-4935-a218-58740cd8f9dc";
+	String url = "https://kr.api.riotgames.com/lol/summoner/v3/summoners/by-name/"+name+"?api_key=" + key;
+	JSONObject obj = new JSONObject();
 }
